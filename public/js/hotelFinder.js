68cb384c408b863838b7ef19ed06d851a37a3bb7
“Nuha” <nuha14m@gmail.com>
2021-08-03 16:32:49 +0000

    Connect to Yelp API and display nearby hotel data

--- public/js/hotelFinder.js
+++ public/js/hotelFinder.js
@@ -1,43 +1,40 @@
 console.log("running");
 
-let businessList = [{
-    name: "Four Barrels Coffee",
-    url: "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
-    location: {
-        city: "San Francisco",
-        state: "CA"
-    },
-    price: "$$"
-},
-{
-    name: "The Hideout Kitchen",
-    url: "https://www.yelp.com/biz/the-hideout-kitchen-lafayette-3?osq=Restaurants",
-    location: {
-        city: "Lafayette",
-        state: "CA"
-    },
-    price: "$$"
-},
-{
-    name: "Postino",
-    url: "https://www.yelp.com/biz/postino-lafayette-2?osq=postinos",
-    location: {
-        city: "Lafayette",
-        state: "CA"
-    },
-    price: "$$$"
-}];
+let queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
 
-// const submitButton = document.querySelector("#submitButton");
-const results = document.querySelector("#results");
+const submitForm = () => {
+    var inputLocation = document.querySelector("#locationText").value
+    $.ajax({
+    url: queryURL,
+    method: "GET",
+    headers: {
+        "accept": "application/json",
+        "x-requested-with": "xmlhttprequest",
+        "Access-Control-Allow-Origin": "*",
+        "Authorization": `Bearer ${apiKey}`
+    },
+    data: {
+        term: 'hotel',
+        location: inputLocation,
+    }
+}).then(function (res) {
+   // let results = res.data
+    console.log(res);
+     $.each(res.businesses, function(i, item) {
+          var id = item.id;
+                       var alias = item.alias;
+                       var phone = item.display_phone;
+                       var image = item.image_url;
+                       var name = item.name;
+                       var rating = item.rating;
+                       var reviewcount = item.review_count;
+                       var address = item.location.address1;
+                       var city = item.location.city;
+                       var state = item.location.state;
+                       var zipcode = item.location.zip_code;
+                       // Append our result into our page
+                       $('#results').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + image + '" style="width:200px;height:150px;"><br>We found <b>' + name + '</b> (' + alias + ')<br>Business ID: ' + id + '<br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
 
-const submitForm = () =>{
-    const inputText = document.querySelector("#inputText");
-    console.log(inputText.value);
-    console.log(businessList);
-    // businessList.forEach(business =>{
-    //     if(business.location.city.value === inputText.value){
-    //         results.innerHTML="<h1>YAY</h1>"
-    //         console.log(business);
-    //     }
+     })
+    });
 }
