4d197e6b5680902f3f6e7923214822d7be0038e0
“Nuha” <nuha14m@gmail.com>
2021-08-05 18:22:06 +0000

    Create login page, display packing list items saved in database

--- public/js/checklist.js
+++ public/js/checklist.js
@@ -1,35 +1,14 @@
-let googleUser = "CihZifpVyNT4E2Phc3paZYQ6Nf62";
+let googleUserId;
 
 //added function here
 const addToDatabase = (input, quantityItem) => {
-    firebase.database().ref(`users/${googleUser}/checklist`).push({
+    firebase.database().ref(`users/${googleUserId}/checklist`).push({
         item: input.value,
         quantity: quantityItem.value
     });
+    console.log("done")
 }
 
-// const deleteItem = (item) => {
-//     firebase.database().ref(`users/${googleUser}/checklist/${item}`).remove();
-// }
-
-
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    this.parentElement.style.display = "none";
-  }
-}
 
 document.querySelector('ul').addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
@@ -38,35 +17,78 @@ document.querySelector('ul').addEventListener('click', function(ev) {
 }, false);
 
 function addElement() {
-  var li = document.createElement("li");
   var myInput = document.querySelector(".myInput");
   var inputQuantity = document.querySelector(".myQuant");
-  if(inputQuantity.value>1){
-    var t = document.createTextNode(`${myInput.value} (${inputQuantity.value})`);
-  }else{
-        var t = document.createTextNode(myInput.value);
-  }
-  addToDatabase(myInput, inputQuantity);
- 
-  li.appendChild(t);
+
+  
   if (myInput.value === '') {
     alert("The input is blank!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
+  } 
+  else{
+      addToDatabase(myInput, inputQuantity);
+      myInput.value = "";
+      inputQuantity.value ="";
   }
-  myInput.value = "";
-  inputQuantity.value ="";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  
 }
+
+window.onload = (event) => {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      console.log('Logged in as: ' + user.displayName);
+      googleUserId = user.uid;
+      console.log(googleUserId)
+      getListItems(googleUserId);
+    } else {
+      // If not logged in, navigate back to login page.
+      window.location = 'login.html';
+    };
+  });
+};
+
+
+const getListItems = (userId) => {
+  const notesRef = firebase.database().ref(`users/${userId}/checklist`);
+  notesRef.on('value', (snapshot) => {
+    const data = snapshot.val();
+    renderDataAsHtml(data);
+  });
+};
+
+
+const renderDataAsHtml = (data) => {
+    var listItems = ``;
+    var li = document.createElement("li");
+    for (const key in data) {
+    const myItem = data[key];
+       listItems += 
+       `
+       <li id=${key}>
+        ${myItem.item} (${myItem.quantity})
+        <span class="close">\u00D7</span>
+        </li>
+        `;
+    }
+    document.querySelector("#myUL").innerHTML=listItems;
+
+
+    var myNodelist = document.getElementsByTagName("LI");
+    var i;
+    for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+    }
+
+    var close = document.querySelectorAll(".close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        this.parentElement.style.display = "none";
+
+    }
+    }
+};
+
